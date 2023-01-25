220cc363496350472d4921980622e5a23c6d0bb9
Ewa <73430438+ewazalewska@users.noreply.github.com>
2023-01-25 22:49:45 +0100

    Add files via upload

---
+++ slice.py
@@ -0,0 +1,8 @@
+# slice[start:stop:step] default: start = 0, stop = the end of the sequence, step = 1
+
+L=[55, 20, -3, 8, 201]
+print(L[::])
+print(L[::-1])
+
+s = "!ldklarsolw6 johljlkeoH"
+print(s[-1::-2])
